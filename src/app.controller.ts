cfcf413e529c7cf646eb9c0f4a9847564e37c3d9
Carlos Eduardo Dos Santos <anjoazrael10@gmail.com>
2022-09-20 08:54:36 -0300

    api  finalizada

---
+++ src/app.controller.ts
@@ -0,0 +1,63 @@
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Req,
+  Request,
+  Res,
+} from '@nestjs/common';
+import { Response } from 'express';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('/games')
+  getGame() {
+    return this.appService.findGameAll();
+  }
+
+  @Post('/games/:id/ads')
+  async getAds(
+    @Req() req: Request,
+    @Param('id') id: string,
+    @Res() response: Response,
+  ) {
+    const body = req.body;
+    const gameId = id;
+    const game = await this.appService.getFindOneGame(gameId);
+
+    if (game === null) {
+      return response.status(404).json({ message: 'Not Found' });
+    }
+
+    return response
+      .status(201)
+      .json(await this.appService.createAds(body, gameId));
+  }
+
+  @Get('/games/:id/ads')
+  async getAd(@Param('id') id: string, @Res() response: Response) {
+    const gameId = id;
+    const ad = await this.appService.getListAds(gameId);
+    console.log(ad);
+    if (ad === null || ad.length === 0) {
+      return response.status(404).json({ message: 'Not Found' });
+    }
+    return response.status(200).json(ad);
+  }
+
+  @Get('/ads/:id/discord')
+  async getDiscord(@Param('id') id: string, @Res() response: Response) {
+    const discord = id;
+    const ad = await this.appService.listDiscord(discord);
+
+    if (ad === null) {
+      return response.status(404).json({ message: 'Not Found' });
+    }
+
+    return response.status(200).json(ad);
+  }
+}
---
+++ src/app.service.ts
@@ -0,0 +1,111 @@
+import { Body, Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+
+@Injectable()
+export class AppService {
+  constructor(private prisma: PrismaService) {}
+  async findGameAll() {
+    return await this.prisma.game.findMany({
+      include: {
+        _count: {
+          select: {
+            ads: true,
+          },
+        },
+      },
+    });
+  }
+
+  async getFindOneGame(gameId) {
+    return this.prisma.game.findFirst({
+      where: {
+        id: gameId,
+      },
+    });
+  }
+
+  async createAds(@Body() body, id: string) {
+    const gameId: string = id;
+    console.log('entrou  aqui => ', body);
+
+    const ads = await this.prisma.ad.create({
+      data: {
+        name: body.name,
+        discord: body.discord,
+        yearsPlaying: body.yearsPlaying,
+        hourEnd: this.ConvertHoursForMinutes(body.hourEnd),
+        hourStart: this.ConvertHoursForMinutes(body.hourStart),
+        weekDays: body.weekDays.toString(),
+        useVoiceChannel: body.useVoiceChannel,
+        gameId,
+      },
+    });
+
+    return ads;
+  }
+
+  ConvertHoursForMinutes(hourString: string) {
+    const [hours, minutes] = hourString.split(':').map(Number);
+    const minutesAmount = hours * 60 + minutes;
+    return minutesAmount;
+  }
+  ConvertMinutesForHours(minutesAmount: number) {
+    const hours = Math.floor(minutesAmount / 60);
+    const minutes = minutesAmount % 60;
+    return (
+      String(hours).padStart(2, '0') + ':' + String(minutes).padStart(2, '0')
+    );
+  }
+
+  async getListAds(gameId: string) {
+    const ad = await this.prisma.ad.findMany({
+      select: {
+        id: true,
+        name: true,
+        yearsPlaying: true,
+        weekDays: true,
+        hourStart: true,
+        hourEnd: true,
+        useVoiceChannel: true,
+      },
+      where: {
+        gameId,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+
+    return ad.map(
+      (arr: {
+        name: any;
+        useVoiceChannel: any;
+        yearsPlaying: any;
+        weekDays: string;
+        hourStart: number;
+        hourEnd: number;
+      }) => {
+        return {
+          name: arr.name,
+          useVoiceChannel: arr.useVoiceChannel,
+          yearsPlaying: arr.yearsPlaying,
+          weekDays: arr.weekDays.split(','),
+          hourStart: this.ConvertMinutesForHours(arr.hourStart),
+          hourEnd: this.ConvertMinutesForHours(arr.hourEnd),
+        };
+      },
+    );
+  }
+
+  async listDiscord(discordName: string) {
+    const ad = await this.prisma.ad.findUnique({
+      select: {
+        discord: true,
+      },
+      where: {
+        id: discordName,
+      },
+    });
+    return ad;
+  }
+}
